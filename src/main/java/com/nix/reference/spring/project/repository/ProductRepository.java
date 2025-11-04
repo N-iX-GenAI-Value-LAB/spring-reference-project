4e25b26c4074926c149272841f63104b90d34fab
Bogdan Solga <bsolga@n-ix.com>
2025-11-04 12:34:48 +0200

    [clean] Not needed

--- src/main/java/com/nix/reference/spring/project/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nix.reference.spring.project.repository;
-
-import com.nix.reference.spring.project.domain.model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-}
--- src/main/java/com/nix/reference/spring/project/repository/SectionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nix.reference.spring.project.repository;
-
-import com.nix.reference.spring.project.domain.model.Section;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SectionRepository extends CrudRepository<Section, Integer> {
-}
